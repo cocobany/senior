da1dd713265e1f0303cdbecac930741f469d2b1c
오지현 <ji-hyun.oh@cnspartner.com>
2025-08-13 13:52:35 +0900

    navigation

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      // colors: {
+      //   primary: "#1A73E8",
+      //   secondary: "#F9AB00",
+      // },
+      // spacing: {
+      //   72: "18rem",
+      //   84: "21rem",
+      // },
+      // zIndex: {
+      //   60: "60",
+      // },
+    },
+  },
+  plugins: [],
+};
